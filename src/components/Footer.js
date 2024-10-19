a4ed34e6a7de46cd9d3086bcd06f3ac85205755f
soraya <sorayaeldjama@gmail.com>
2024-10-19 19:51:52 +0200

    commit github href

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -43,7 +43,7 @@ function Footer() {
             </li> */}
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/soraya-el-djama/"
+                href="https://www.linkedin.com/in/soraya-el-djama"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"

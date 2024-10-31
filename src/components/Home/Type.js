bac42b55ae9df97e58410b9d5359e4fed82b5ca8
soraya <sorayaeldjama@gmail.com>
2024-10-31 14:42:24 +0100

    modfier

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,10 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Développeuse Full Stack",
+          "",
+          "",
+          "",
         ],
         autoStart: true,
         loop: true,

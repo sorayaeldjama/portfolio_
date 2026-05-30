88c4b882b2b6e3b5ae5783e4130b9d954d50cdf4
soraya <sorayaeldjama@gmail.com>
2026-05-30 18:40:30 +0200

    fix: nettoyage des imports inutilisés

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,28 +1,17 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
 import {
   DiJavascript1,
   DiReact,
   DiNodejs,
-  DiMongodb,
-  DiPython,
-  DiGit,
   DiJava
 ,
 } from "react-icons/di";
 import {
-  SiRedis,
-  SiFirebase,
   SiNextdotjs,
-  SiSolidity,
   SiPostgresql,
   SiAngular,  SiTypescript,SiMysql,SiSpringboot 
-
-
-
 } from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
   return (
@@ -46,28 +35,6 @@ function Techstack() {
        <Col xs={4} md={2} className="tech-icons">
         <  SiAngular />
       </Col>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-      </Col> */}
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <TbBrandGolang />
-      </Col> */}
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiSolidity />
-      </Col> */}
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col> */}
-     
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col> */}
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiRedis />
-      </Col> */}
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-        </Col> */}
       <Col xs={4} md={2} className="tech-icons">
         <DiJava />
       </Col>
